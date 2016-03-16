279b4c6b4cb6eee5c807dc097d3371906bb1dbc7
Bin Xia <binxi@microsoft.com>
2016-03-16 12:27:00 +0000

    Pass credentials and subscriptionId to listeners

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 
 var async = require('async');
 
-var Common = require('./lib/common');
+var common = require('./lib/common');
 var Broker = require('./lib/broker');
-var Config = require('./config/meta-service-broker');
-
-Common.validateEnvironmentVariables();
-var broker = new Broker(Config);
-broker.start();
-
+var config = require('./config/meta-service-broker');
 var echo = require('./lib/services/echo')
 var azurestorageblob = require('./lib/services/azurestorageblob')
+
+var broker = new Broker(config);
+
+broker.log.info(
+  'Validating and getting Azure credentials and subscript ID from environment variables...'
+);
+common.validateEnvironmentVariables();
+var credentialsAndSubscriptionId = common.getCredentialsAndSubscriptionId();
+var credentials = credentialsAndSubscriptionId.credentials;
+var subscriptionId = credentialsAndSubscriptionId.subscriptionId;
 
 var addListeners = function(serviceId, serviceModule) {
   broker.on('provision-' + serviceId, serviceModule.provision);
@@ -27,16 +32,18 @@ broker.log.info(
 
 async.parallel([
     function(callback) {
-      echo.catalog({}, broker.log, function(err, result) {
+      echo.catalog(credentials, subscriptionId, {}, broker.log, function(err,
+        result) {
         addListeners(result.id, echo);
         callback(null, result);
       });
     },
     function(callback) {
-      azurestorageblob.catalog({}, broker.log, function(err, result) {
-        addListeners(result.id, azurestorageblob);
-        callback(null, result);
-      });
+      azurestorageblob.catalog(credentials, subscriptionId, {}, broker.log,
+        function(err, result) {
+          addListeners(result.id, azurestorageblob);
+          callback(null, result);
+        });
     }
   ],
   function(err, results) {
@@ -47,3 +54,5 @@ async.parallel([
       next(reply);
     });
   });
+
+broker.start();
