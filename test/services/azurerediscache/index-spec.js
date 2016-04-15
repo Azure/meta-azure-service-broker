d83a851cf023f44f9524b3c831dc5b4fdd8aee19
Greg Oliver <golive@microsoft.com>
2016-04-14 20:06:06 -0700

    eliminate dependency on environment variables

--- test/services/azurerediscache/index-spec.js
+++ test/services/azurerediscache/index-spec.js
@@ -7,13 +7,19 @@ var logule = require('logule');
 var should = require('should');
 var sinon = require('sinon');
 var uuid = require('node-uuid');
-var common = require('../../../lib/common');
 var service = require('../../../lib/services/azurerediscache/service.json');
 var handlers = require('../../../lib/services/azurerediscache/index');
 var redisClient = require('../../../lib/services/azurerediscache/client');
 var resourceGroupClient = require('../../../lib/common/resourceGroup-client');
 
-var azure = common.getCredentialsAndSubscriptionId();
+var azure = {
+    environment: 'AzureCloud',
+    subscription_id: '743fxxxx-83xx-46xx-xx2d-xxxxb953952d',
+    tenant_id: '72xxxxbf-8xxx-xxxf-9xxb-2d7cxxxxdb47',
+    client_id: 'd8xxxx18-xx4a-4xx9-89xx-9be0bfecxxxx',
+    client_secret: '2/DzYYYYYYYYYYsAvXXXXXXXXXXQ0EL7WPxEXX115Go=',
+};
+  
 var log = logule.init(module, 'Redis Cache-Tests');
 var generatedValidInstanceId = uuid.v4();
 var provisioningResult = "{\"id\":\"/subscriptions/" + azure.subscription_id + "/resourceGroups/redisResourceGroup/providers/Microsoft.Cache/Redis/C0CacheE\",\"name\":\"C0CacheE\",\"type\":\"Microsoft.Cache/Redis\",\"location\":\"East US\",\"tags\":{},\"accessKeys\":{\"primaryKey\":\"4eEobxjSUnBjAHYWGO+0M69/XikkJv6+EPiaXMjfNJg=\",\"secondaryKey\":\"Zb3e6FZAwzJS60eBbN7sLTFp76UdWfhFno99Pal/dL0=\"},\"provisioningState\":\"Creating\",\"hostName\":\"C0CacheE.redis.cache.windows.net\",\"port\":6379,\"sslPort\":6380,\"redisVersion\":\"3.0\",\"sku\":{\"name\":\"Basic\",\"family\":\"C\",\"capacity\":0},\"redisConfiguration\":{\"maxclients\":\"256\",\"maxmemory-reserved\":\"2\",\"maxmemory-delta\":\"2\"},\"enableNonSslPort\":false}";
--- test/services/azurerediscache/poll-spec.js
+++ test/services/azurerediscache/poll-spec.js
@@ -11,13 +11,19 @@ var _ = require('underscore');
 var logule = require('logule');
 var should = require('should');
 var sinon = require('sinon');
-var common = require('../../../lib/common');
 var cmdPoll = require('../../../lib/services/azurerediscache/cmd-poll');
 var redisClient = require('../../../lib/services/azurerediscache/client');
 
 var log = logule.init(module, 'RedisCache-Mocha');
 
-
+var azure = {
+    environment: 'AzureCloud',
+    subscription_id: '743fxxxx-83xx-46xx-xx2d-xxxxb953952d',
+    tenant_id: '72xxxxbf-8xxx-xxxf-9xxb-2d7cxxxxdb47',
+    client_id: 'd8xxxx18-xx4a-4xx9-89xx-9be0bfecxxxx',
+    client_secret: '2/DzYYYYYYYYYYsAvXXXXXXXXXXQ0EL7WPxEXX115Go=',
+};
+  
 describe('RedisCache - Poll - PreConditions', function() {
     var validParams;
         
@@ -32,7 +38,7 @@ describe('RedisCache - Poll - PreConditions', function() {
             provisioning_result: '{\"provisioningState\":\"Creating\"}',
             last_operation : "provision",
         };
-        validParams.azure = common.getCredentialsAndSubscriptionId();
+        validParams.azure = azure;
     });
     
     describe('Poll should succeed if ...', function() {
@@ -57,7 +63,7 @@ describe('RedisCache - Poll - Execution - Cache that exists', function() {
             },
             provisioning_result: '{\"provisioningState\":\"Creating\"}'
         };
-        validParams.azure = common.getCredentialsAndSubscriptionId();
+        validParams.azure = azure;
     });
     
     after(function() {
