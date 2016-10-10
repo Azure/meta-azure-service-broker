76674e5668ef9d8fa6cead477185ead3540f27e5
Bin Xia <binxi@microsoft.com>
2016-10-10 06:44:48 +0000

    Add a credential for azure-sqldb test case

--- test/integration/test-matrix.js
+++ test/integration/test-matrix.js
@@ -66,44 +66,6 @@ testMatrix.push(azureservicebus);
 
 instanceId = uuid.v4();
 bindingId = uuid.v4();
-resourceGroupName = 'cloud-foundry-' + instanceId;
-var cacheName = 'cf' + instanceId;
-var azurerediscache = {
-  serviceName: 'azure-rediscache',
-  serviceId: '0346088a-d4b2-4478-aa32-f18e295ec1d9',
-  planId: '362b3d1b-5b57-4289-80ad-4a15a760c29c',
-  instanceId: instanceId,
-  bindingId: bindingId,
-  provisioningParameters: {
-    "resourceGroup": resourceGroupName,
-    "cacheName": cacheName,
-    "parameters": {
-      "location": "eastus",
-      "enableNonSslPort": false,
-      "sku": {
-        "name": "Basic",
-        "family": "C",
-        "capacity": 0
-      },
-      "tags": {
-        "foo": "bar"
-      }
-    }
-  },
-  bindingParameters: {},
-  credentials: {
-    "hostname": cacheName + ".redis.cache.windows.net",
-    "name": cacheName,
-    "port": 6379,
-    "primaryKey": "<string>",
-    "secondaryKey": "<string>",
-    "sslPort": 6380
-  }
-}
-testMatrix.push(azurerediscache);
-
-instanceId = uuid.v4();
-bindingId = uuid.v4();
 var azuredocumentdb = {
   serviceName: 'azure-documentdb',
   serviceId: '3befc561-4f0c-4df3-ab26-48ac4e366b1c',
@@ -174,9 +136,48 @@ var azuresqldb = {
     "administratorLogin": "<string>",
     "administratorLoginPassword": "<string>",
     "sqlServerName": sqlServerName,
-    "sqldbName": sqldbName
+    "sqldbName": sqldbName,
+    "jdbcUrl": "<string>"
   }
 }
 testMatrix.push(azuresqldb);
+
+instanceId = uuid.v4();
+bindingId = uuid.v4();
+resourceGroupName = 'cloud-foundry-' + instanceId;
+var cacheName = 'cf' + instanceId;
+var azurerediscache = {
+  serviceName: 'azure-rediscache',
+  serviceId: '0346088a-d4b2-4478-aa32-f18e295ec1d9',
+  planId: '362b3d1b-5b57-4289-80ad-4a15a760c29c',
+  instanceId: instanceId,
+  bindingId: bindingId,
+  provisioningParameters: {
+    "resourceGroup": resourceGroupName,
+    "cacheName": cacheName,
+    "parameters": {
+      "location": "eastus",
+      "enableNonSslPort": false,
+      "sku": {
+        "name": "Basic",
+        "family": "C",
+        "capacity": 0
+      },
+      "tags": {
+        "foo": "bar"
+      }
+    }
+  },
+  bindingParameters: {},
+  credentials: {
+    "hostname": cacheName + ".redis.cache.windows.net",
+    "name": cacheName,
+    "port": 6379,
+    "primaryKey": "<string>",
+    "secondaryKey": "<string>",
+    "sslPort": 6380
+  }
+}
+testMatrix.push(azurerediscache);
 
 module.exports = testMatrix;
