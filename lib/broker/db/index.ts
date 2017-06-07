2cf86d42d0ffd2edcf59779b264622af5698421d
Aaron Schlesinger <aaschles@microsoft.com>
2017-06-07 16:33:43 -0700

    Refactoring a few files in Typescript

--- lib/broker/db/index.ts
+++ lib/broker/db/index.ts
@@ -1,12 +1,9 @@
-'use strict';
 var Sqlserver = require('./sqlserver');
 
-var Database = function(opts) {
-  var db = new Sqlserver(opts);
+export function Database(opts: any) {
+  let db = new Sqlserver(opts);
 
   db.instanceTableName = 'instances';
   db.bindingTableName = 'bindings';
   return db;
-};
-
-module.exports = Database;
+}
