e3cb9422d0e06af0630a98e7ab558db290852969
Zhongyi Zhang <zhongz@microsoft.com>
2018-06-04 13:29:09 +0800

    add sqldb plans for s4 - s12

--- test/integration/test-matrix2.js
+++ test/integration/test-matrix2.js
@@ -72,4 +72,50 @@ azuresqldb = {
 };
 testMatrix.push(azuresqldb);
 
+instanceId = uuid.v4();
+bindingId = uuid.v4();
+sqldbName = 'cf' + instanceId;
+azuresqldb = {
+  serviceName: 'azure-sqldb',
+  serviceId: 'fb9bc99e-0aa9-11e6-8a8a-000d3a002ed5',
+  planId: 'a24192ce-b445-4868-b648-f02cdbe55830',
+  instanceId: instanceId,
+  bindingId: bindingId,
+  provisioningParameters: {
+    'sqlServerName': sqlServerName,
+    'sqldbName': sqldbName,
+    'transparentDataEncryption': true,
+    'sqldbParameters': {
+      'properties': {
+        'collation': 'SQL_Latin1_General_CP1_CI_AS'
+      },
+      'tags': {
+        'foo': 'bar'
+      }
+    }
+  },
+  envProvisioningParameters: {
+    administratorLogin: administratorLogin,
+    administratorLoginPassword: administratorLoginPassword
+  },
+  bindingParameters: {},
+  credentials: {
+    'databaseLogin': '<string>',
+    'databaseLoginPassword': '<string>',
+    'sqlServerName': sqlServerName,
+    'sqlServerFullyQualifiedDomainName': '<string>',
+    'sqldbName': sqldbName,
+    'jdbcUrl': '<string>',
+    'jdbcUrlForAuditingEnabled': '<string>',
+    'hostname': '<string>',
+    'port': 1433,
+    'name': '<string>',
+    'username': '<string>',
+    'password': '<string>',
+    'uri': '<string>'
+  },
+  e2e: true
+};
+testMatrix.push(azuresqldb);
+
 module.exports = testMatrix;
